5358354a00c1d5a683e0fbaefa155cc74f5d671e
artyom <admitriev5813@gmail.com>
2018-03-24 17:57:28 +0300

    Controller cleanup

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,28 +1,20 @@
 class RequestsController < ApplicationController
   before_action :set_request, only: [:show, :edit, :update, :destroy]
 
-  # GET /requests
-  # GET /requests.json
   def index
     @requests = Request.all
   end
 
-  # GET /requests/1
-  # GET /requests/1.json
   def show
   end
 
-  # GET /requests/new
   def new
     @request = Request.new
   end
 
-  # GET /requests/1/edit
   def edit
   end
 
-  # POST /requests
-  # POST /requests.json
   def create
     @request = Request.new(request_params)
 
@@ -37,8 +29,6 @@ class RequestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /requests/1
-  # PATCH/PUT /requests/1.json
   def update
     respond_to do |format|
       if @request.update(request_params)
@@ -51,8 +41,6 @@ class RequestsController < ApplicationController
     end
   end
 
-  # DELETE /requests/1
-  # DELETE /requests/1.json
   def destroy
     @request.destroy
     respond_to do |format|
@@ -67,12 +55,10 @@ class RequestsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_request
       @request = Request.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def request_params
       params.require(:request).permit(:problem_desc, :first_name, :last_name, :phone_number)
     end
