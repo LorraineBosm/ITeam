9dcab033e1d2a39a65a270f3ce9f52a6a5199818
artyom <admitriev5813@gmail.com>
2018-03-24 17:47:53 +0300

    Add validations for Request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,10 @@
 class Request < ApplicationRecord
   belongs_to :device_model
+
+  validates :problem_desc, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :device_model_id, presence: true
+
 end
