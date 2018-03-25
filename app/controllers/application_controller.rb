60e94f7ed28e4b7a02bf2879c63dd165a11c3bf9
artyom <admitriev5813@gmail.com>
2018-03-25 17:03:30 +0300

    Add check if User has an acceptor role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def is_acceptor?
+    redirect_to root_path if current_user.nil? || !current_user.acceptor?
+  end
 end
