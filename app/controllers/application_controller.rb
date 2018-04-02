afcac060c0456ba67628ee1626808357cf0be567
artyom <admitriev5813@gmail.com>
2018-04-02 20:04:30 +0300

    Update controllers according to new Role model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def has_role? role
-    redirect_to root_path if current_user.nil? || !current_user.roles.any? {|r| r.name == role}
+    if current_user.nil? || !current_user.roles.any? {|r| r.name == role}
+      redirect_to root_path
+    else
+      true
+    end
   end
+
+  helper_method :has_role?
 end
