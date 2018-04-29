a6e2555652e6dd68878bc22a4af630b6d284168e
artyom <admitriev5813@gmail.com>
2018-04-29 19:33:59 +0300

    Implement basic analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,15 @@
 class AnalyticsController < ApplicationController
   def index
-    
+  end
+
+  def requests
+    @requests = Request.includes(device_model: [device_brand: [:device_type]])
+  end
+
+  def agreements
+    @agreements = Agreement.includes(device_model: [device_brand: [:device_type]])
+  end
+
+  def users
   end
 end
