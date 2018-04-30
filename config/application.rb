ff591c5d8dcd9f36d24aa80578e14d413ff09b02
artyom <admitriev5813@gmail.com>
2018-04-30 13:51:21 +0300

    Add navbar & Devise translations

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module ITeam
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    I18n.config.available_locales = :ru
+    config.i18n.default_locale = :ru
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*', '*.{rb,yml}')]
   end
 end
